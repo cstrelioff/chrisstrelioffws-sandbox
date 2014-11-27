a5fdb34d533990bed06e2fcc40107bf229a4ea86
Christopher C Strelioff <chris.strelioff@gmail.com>
2014-11-26 16:01:07 -0800

    rm deleted/moved files

--- 2014/11/19/pweave.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-#
-# pweave.sh
-# Copyright (C) 2014 Christopher C. Strelioff <chris.strelioff@gmail.com>
-#
-# Distributed under terms of the MIT license.
-#
-pweave -f sphinx --figure-directory figs inferring_probabilities_with_a_beta_prior_a_third_example_of_bayesian_calculations.Pnw 
-ptangle inferring_probabilities_with_a_beta_prior_a_third_example_of_bayesian_calculations.Pnw
